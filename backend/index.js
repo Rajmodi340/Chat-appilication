f21bd5c7da806b1ab5401dc6de89dfacb906570c
Raj Modi <modiraj340@gmail.com>
2025-11-23 23:54:35 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chat-appilication-2.onrender.com",
     credentials:true
 }))
 app.use(express.json())
